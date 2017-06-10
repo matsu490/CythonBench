c5fc61a709eddc038851e3288be1d393f5d78f55
matsu490 <imototoriwatomodatijanai@gmail.com>
2017-06-10 22:58:34 +0900

    show informations about platform

--- main.py
+++ main.py
@@ -7,9 +7,10 @@
 #
 # Copyright (C) 2017 Taishi Matsumura
 #
+import platform
+import matplotlib.pyplot as plt
 import module
 import cythonized_module
-import matplotlib.pyplot as plt
 
 plt.close('all')
 
@@ -28,13 +29,16 @@ x = xrange(3)
 y = [s.laps[1][0], s.laps[2][0], s.laps[3][0]]
 labels = ['Pure python', 'Cython', 'Cython\n+cdef']
 text = '''
-    Task 1
-    ({} loops)
-    '''.format(N**2)
+    Task 1 ({0} loops)
+
+    Platform : {1}
+    Version : {2}
+    CPU : {3}
+    '''.format(N**2, platform.system(), platform.release(), platform.processor())
 
 f = plt.figure()
 ax = f.add_subplot(111)
 ax.bar(x, y, align='center', tick_label=labels, width=0.5)
 ax.set_ylabel('Elapsed time [sec]')
-ax.text(0.7*max(x), 0.8*max(y), text)
+ax.text(0.5*max(x), max(y), text, ha='left', va='top')
 f.show()
